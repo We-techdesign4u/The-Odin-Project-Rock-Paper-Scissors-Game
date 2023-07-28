dad6940d25ef61fa80f52e738d4cd91316ac15b0
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-07-28 04:28:31 +0100

    round of 2 running

--- script.js
+++ script.js
@@ -13,25 +13,29 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection1, computerSelection1) {
   // your code here!
-  if (playerSelection == computerSelection) {
+  if (playerSelection1 == computerSelection1) {
     return "it's a tie";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
+  } else if (playerSelection1 === "rock" && computerSelection1 === "paper") {
     return "You lose, paper beats rock";
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+  } else if (playerSelection1 == "rock" && computerSelection1 == "scissors") {
     return "You win, rock beats scissors";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
+  } else if (playerSelection1 == "paper" && computerSelection1 == "rock") {
     return "You win, paper beats rock";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+  } else if (playerSelection1 == "paper" && computerSelection1 == "scissors") {
     return "You lose, scissors beats paper";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+  } else if (playerSelection1 == "scissors" && computerSelection1 == "paper") {
     return "You win, scissors beats paper";
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+  } else if (playerSelection1 == "scissors" && computerSelection1 == "rock") {
     return "You lose, rock beats scissors";
   }
 }
 
-const playerSelection = prompt("Choose your weapon: rock, paper or scissors?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+const playerSelection1 = prompt("Choose your weapon: rock, paper or scissors?");
+const computerSelection1 = getComputerChoice();
+alert(playRound(playerSelection1, computerSelection1));
+
+const playerSelection2 = prompt("Choose your weapon: rock, paper or scissors?");
+const computerSelection2 = getComputerChoice();
+alert(playRound(playerSelection2, computerSelection2));
